37253ad11a670a96f0b9fcdd0e8202aaf0d9d4ec
janetxinli <janetxinli@gmail.com>
2020-11-26 19:34:29 -0800

    add final touches to script

--- parse_variants.py
+++ parse_variants.py
@@ -3,6 +3,7 @@
 import re
 import sys
 import argparse
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -31,6 +32,17 @@ def read_gene_variants(filename):
     return variants
 
 
+def count_variant_types(variants):
+    """Returns a dataframe with columns [type of variant, count]."""
+    variant_types = []
+    variant_counts = []
+    for v in variants:
+        variant_types.append(v)
+        variant_counts.append(len(variants[v]))
+    var_df = pd.DataFrame(zip(variant_types, variant_counts), columns=["type", "count"]).sort_values(by="count", ascending=False)
+    return var_df
+
+
 def read_qs_genes(go_gene_file):
     """
     Returns a dictionary of Pseudomonas genes with a certain GO accession from a tsv file
@@ -45,24 +57,54 @@ def read_qs_genes(go_gene_file):
     return gene_info
 
 
-def print_qs_variants(variants, qs_genes, outfile):
+def get_qs_variants(variants, qs_genes, prefix):
     """
     Prints variants in query_genes that are in qs_genes in tsv format to outfile.
+    Also returns a list of mutation types corresponding to the qs gene variants.
     Format: locus_id    gene_name   mutation_type   product_description
     """
-    if outfile is None:
-        o = sys.stdout
-    else:
-        o = open(outfile, "w+")
-    print("locus_id\tgene_name\tmutation_type\tproduct_description", file=o)
-    for var_type in variants:
-        for locus in variants[var_type]:
-            if locus in qs_genes:
-                print(locus, qs_genes[locus][0], var_type, qs_genes[locus][1], sep="\t", file=o)
+    outfile = prefix + ".qs_variants.tsv"
+    mutation_type = []
+    # Check if file is writable
+    with open(outfile, "w+") as o:
+        print("locus_id\tgene_name\tmutation_type\tproduct_description", file=o)
+        for var_type in variants:
+            for locus in variants[var_type]:
+                if locus in qs_genes:
+                    mutation_type.append(var_type)
+                    print(locus, qs_genes[locus][0], var_type, qs_genes[locus][1], sep="\t", file=o)
+    return mutation_type
 
 
-def plot_variants(variants):
+def plot_var_counts(variant_counts, prefix):
+    """Prints a bar plot of variant counts to a file."""
+    outfile = prefix + ".variant_types.png"
+    # Check if file is writable
+    bar = sns.barplot(y=variant_counts["type"], x=variant_counts["count"],
+        color="grey", label="big")
+    bar.set(xscale="log")
+    bar.set_xlabel(xlabel="Count", fontsize=15)
+    bar.set_ylabel(ylabel="Variant Type", fontsize=15)
+    bar.set_title("Variant Types and Frequencies", fontsize=20)
+    bar.tick_params(labelsize=15)
+    bar_plot = bar.get_figure()
+    bar_plot.savefig(outfile, bbox_inches="tight")
+    bar_plot.close()
 
+
+def plot_qs_variants(qs_variants, prefix):
+    """Prints a histogram of variant types in quorum sensing genes."""
+    outfile = prefix + ".qs_variants.png"
+    # Check if file is writable
+    qs_var_df = pd.DataFrame(qs_variants, columns=["mutation_type"])
+    qs = sns.histplot(qs_var_df, y="mutation_type", color="grey", alpha=1)
+    qs.set_title("Types of Quorum Sensing Gene Variants", fontsize=20)
+    qs.set_xlabel("Count", fontsize=15)
+    qs.set_ylabel("Variant Type", fontsize=15)
+    qs.tick_params(labelsize=15)
+    qs_plot = qs.get_figure()
+    qs_plot.savefig(outfile, bbox_inches="tight")
+    qs_plot.close()
 
 
 def get_args():
@@ -74,23 +116,29 @@ def get_args():
     parser.add_argument("qs",
                         type=str,
                         help="Tsv file containing PAO1 genes with the 'quorum sensing' GO accession")
-    parser.add_argument("-o", "--outfile",  # Change to prefix for all files
+    parser.add_argument("-p", "--prefix",
                         type=str,
-                        default=None,
-                        help="Output file for quorum sensing variants to be printed to [stdout]")
+                        default="PAO1",
+                        help="Prefix for output files to be printed to [PAO1]")
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    pao_qs_genes = read_qs_genes(args.qs)
+    # Parse all variants
     genes_with_variants = read_gene_variants(args.vars)
-    print_qs_variants(genes_with_variants, pao_qs_genes, args.outfile)
+    # Get variant counts
+    variant_counts = count_variant_types(genes_with_variants)
+    sns.set_theme(style="whitegrid")
+    # Plot variant counts
+    # plot_var_counts(variant_counts, args.prefix)
+    # Find QS genes with variants
+    qs_gene_variants = read_qs_genes(args.qs)
+    # Print to tsv file
+    qs_variant_counts = get_qs_variants(genes_with_variants, qs_gene_variants, args.prefix)
+    # Plot QS variants
+    plot_qs_variants(qs_variant_counts, args.prefix)
 
 
 if __name__ == "__main__":
     main()
-
-# Add plotting
-# Print all variant types to file
-# Make stacked bar chart
